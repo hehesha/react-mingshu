342710bee0f29cd098e25246ac2feec2fb090f9e
hehesha <634450448@qq.com>
2018-02-03 16:18:01 +0800

    旅游推荐

--- app/src/components/discover/discoverComponent.js
+++ app/src/components/discover/discoverComponent.js
@@ -1,10 +1,25 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import router from 'react-router'
+import StrategyComponent from './strategy/strategy.js'
+import './discoverComponent.scss'
+import {Link} from 'react-router'
 
 export default class HomeComponent extends Component{
     render(){
         return (
-            <div>
-                discover
+            <div className="discover">
+                <div className="n_nav">
+                    <ul className="nav">
+                        <li>发现</li>
+                        <li>房东故事</li>
+                        <li>旅行故事</li>
+                        <li><Link to="discover/strategy">旅游攻略</Link></li>
+                        <li>出行专题</li>
+                        <li>热门评论</li>
+                    </ul>
+                </div>
+                <div className="d_main">{this.props.children}</div>
+                
             </div>
         )
     }
---
+++ app/src/components/discover/strategy/strategy.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react'
+import './strategy.scss'
+
+export default class StrategyComponent extends Component{
+    render(){
+        return (
+            <div>
+                <div className="main">
+                    <ul className="pic">
+                        <li><u>01</u></li>
+                        <li><u>02</u></li>
+                        <li><u>03</u></li>
+                    </ul>
+                    <h3 className="clearfix"><span>攻略</span><b>皇城根下享受老北京生活 </b></h3>
+                    <p>
+                        <b>2018/1/9</b>
+                        <span><i className="empty heart icon"></i>687</span>                    
+                        <span><i className="talk outline icon"></i>1</span>           
+                    </p>
+                </div>
+                <div className="main">
+                    <ul className="pic">
+                        <li><u>01</u></li>
+                        <li><u>02</u></li>
+                        <li><u>03</u></li>
+                    </ul>
+                    <h3 className="clearfix"><span>攻略</span><b>皇城根下享受老北京生活 </b></h3>
+                    <p>
+                        <b>2018/1/9</b>
+                        <span><i className="empty heart icon"></i>687</span>                    
+                        <span><i className="talk outline icon"></i>1</span>           
+                    </p>
+                </div>
+                 <div className="main">
+                    <ul className="pic">
+                        <li><u>01</u></li>
+                        <li><u>02</u></li>
+                        <li><u>03</u></li>
+                    </ul>
+                    <h3 className="clearfix"><span>攻略</span><b>皇城根下享受老北京生活 </b></h3>
+                    <p>
+                        <b>2018/1/9</b>
+                        <span><i className="empty heart icon"></i>687</span>                    
+                        <span><i className="talk outline icon"></i>1</span>           
+                    </p>
+                </div>
+                <div className="main">
+                    <ul className="pic">
+                        <li><u>01</u></li>
+                        <li><u>02</u></li>
+                        <li><u>03</u></li>
+                    </ul>
+                    <h3 className="clearfix"><span>攻略</span><b>皇城根下享受老北京生活 </b></h3>
+                    <p>
+                        <b>2018/1/9</b>
+                        <span><i className="empty heart icon"></i>687</span>                    
+                        <span><i className="talk outline icon"></i>1</span>           
+                    </p>
+                </div>
+            </div>
+        )
+    }
+}
