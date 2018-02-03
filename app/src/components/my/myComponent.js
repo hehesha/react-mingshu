750767d79f561e9125968e8cfc77d037c3cfa724
danielyulu <645345238@qq.com>
2018-02-03 16:23:55 +0800

    头部

--- app/src/components/my/myComponent.js
+++ app/src/components/my/myComponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 
+
 export default class HomeComponent extends Component{
     render(){
         return (
--- app/src/components/recommend/recommendComponent.js
+++ app/src/components/recommend/recommendComponent.js
@@ -1,10 +1,37 @@
 import React, {Component} from 'react'
+import './recommend.scss'
 
 export default class HomeComponent extends Component{
+    componentDidMount(){
+
+    }    
     render(){
         return (
-            <div>
-                recommend
+            <div className="recommend">
+                <div className="head">
+                    <div className="where_to_go">
+                        想去哪?
+                        <span>您要入住的城市或目的地</span>
+                        <div className="searchs">
+                            <i className="search icon" data-filtered="filtered"></i>
+                            搜索目的地
+                        </div>
+                    </div>
+                </div>
+                <div className="main">
+                    <div className="rooms">
+                        <span>一间房一种生活</span>
+                        <div className="photos">
+                            <ul>
+                                <li><img src="../../../assets/01.jpg"/></li>
+                                <li><img src="../../../assets/01.jpg"/></li>
+                                <li><img src="../../../assets/01.jpg"/></li>
+                                <li><img src="../../../assets/01.jpg"/></li>
+                            </ul>
+                        </div>
+                        <div className="parting_line"></div>
+                    </div>
+                </div>
             </div>
         )
     }
