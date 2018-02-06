5867dfa47da1f86ae0caaaf80488465ecc07c315
boa <281189459@qq.con>
2018-02-06 20:22:39 +0800

    解决冲突

--- BM/src/utils/httpclient.js
+++ BM/src/utils/httpclient.js
@@ -1,7 +1,7 @@
 import http from 'superagent'
 
-// let baseUrl = 'http://10.3.136.153:3000/'
-let baseUrl="http://localhost/"
+let baseUrl = 'http://10.3.136.153:3000/'
+//let baseUrl="http://localhost/"
 const geturl = (url) => {
     if(url.startsWith('http')){
         return url;
@@ -15,13 +15,8 @@ export default {
     get(url, params){
         return new Promise((resolve, reject) => {
             http.get(geturl(url))
-<<<<<<< HEAD
-            .set('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
-            .send(params)
-=======
             //传参用query，不然传不过去
             .query(params)
->>>>>>> da9eb88d5d09ef2b9e69901e911adc624177245a
             .end((error, res) => {
                 if(error){
                     reject(error)
