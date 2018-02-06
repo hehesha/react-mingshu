1869b9c5e440c47a7891d7e3693a1eed05b25c80
WONGCHIKIN <850390116@qq.com>
2018-02-06 21:07:00 +0800

    走之前提交一下

--- app/src/components/discover/story/story.js
+++ app/src/components/discover/story/story.js
@@ -2,42 +2,39 @@ import React from 'react';
 import './story.scss'
 import {connect} from 'react-redux'
 import * as actions from '../../../actions/storyAction';
+import {hashHistory} from 'react-router'
 
 class Story extends React.Component{
     componentWillMount(){
-        this.props.getstrategy()
+        this.props.getstory()
+    }
+    btnClick(n){
+        hashHistory.push({  
+            pathname: '/sare',
+            query:{
+                id:n,
+            },  
+            
+        })      
     }
     render(){
         return(
                 <div className="story">
-                    {/* <div className="box">
-                        <img src="../../../../assets/04.jpg" />
-                        <div>
-                            <span>故事</span>
-                            <p>南方人的北京卤煮初体验</p>
-                            <p>
-                                <b>2018/1/9</b>
-                                <span><i className="empty heart icon"></i>687</span>                    
-                                <span><i className="talk outline icon"></i>1</span>           
-                            </p>
-                        </div>
-                   </div> */}
-                   {
-                    this.props.ajaxResult.map(item=>{
-                        return (<div className="box" key={item.id}>
-                            <img src={item.imgurls.split(';')[0]} />
-                            <div>
-                                <span>故事</span>
-                                <p>{item.title}</p>
-                                <p>
-                                    <b>2018/1/9</b>
-                                    <span><i className="empty heart icon"></i>{item.commons}</span>                    
-                                    <span><i className="talk outline icon"></i>{item.likes}</span>           
-                                </p>
-                            </div>
-                        </div>)
-                    })
-                }
+                   {this.props.ajaxResult.map(item=>{
+                        return (<div className="box" key={item.id} onClick={this.btnClick.bind(this,item.id)}>
+                                <img src={item.imgurls.split(';')[0]} />
+                                <div>
+                                    <span>故事</span>
+                                    <p>{item.title}</p>
+                                    <p>
+                                        <b>2018/1/9</b>
+                                        <span><i className="empty heart icon"></i>{item.commons}</span>                    
+                                        <span><i className="talk outline icon"></i>{item.likes}</span>           
+                                    </p>
+                                </div>
+                            </div>)
+                        })
+                    }
                 </div>
             )
     }
