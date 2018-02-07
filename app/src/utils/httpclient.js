d84950d45e5e9015405349616f3577de4ad6f080
laijunhao <790313927@qq.com>
2018-02-07 11:09:54 +0800

    a

--- app/src/utils/httpclient.js
+++ app/src/utils/httpclient.js
@@ -1,11 +1,9 @@
 import http from 'superagent'
 
-<<<<<<< HEAD
 let baseUrl = 'http://10.3.136.153:3000/'
-=======
+
 // let baseUrl = 'http://10.3.136.153:3000/'
-let baseUrl ='http://localhost:3000/'
->>>>>>> a9c4aa2d785d5a88605e2250d562fbd90043c615
+
 const geturl = (url) => {
     if(url.startsWith('http')){
         return url;
